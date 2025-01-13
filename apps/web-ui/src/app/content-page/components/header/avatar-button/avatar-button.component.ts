b646eae8525451c5ec40bfdafb64489592b7f3e9
Emilio K <emiliokartono@gmail.com>
2025-01-13 13:30:32 -0800

    feat: remove default avatar

--- apps/web-ui/src/app/content-page/components/header/avatar-button/avatar-button.component.ts
+++ apps/web-ui/src/app/content-page/components/header/avatar-button/avatar-button.component.ts
@@ -5,9 +5,6 @@ import { NgClickOutsideDirective } from 'ng-click-outside2';
 
 import { authState } from '../../../../auth/store';
 
-const DEFAULT_PROFILE_URL =
-  'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80';
-
 @Component({
   standalone: true,
   selector: 'app-header-avatar-button',
@@ -18,9 +15,7 @@ export class AvatarButtonComponent {
   private readonly store = inject(Store);
 
   readonly isDropdownVisible = signal(false);
-  readonly profileUrl =
-    this.store.selectSignal(authState.selectUserProfileUrl) ||
-    DEFAULT_PROFILE_URL;
+  readonly profileUrl = this.store.selectSignal(authState.selectUserProfileUrl);
 
   toggleDropdown() {
     this.isDropdownVisible.set(!this.isDropdownVisible());
