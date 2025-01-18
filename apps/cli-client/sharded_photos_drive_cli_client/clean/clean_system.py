df630d6301211fdaf3a8be13546dcdbef43310c6
Emilio K <emiliokartono@gmail.com>
2025-01-18 14:26:38 -0800

    feat: wrote tests for clean script

--- apps/cli-client/sharded_photos_drive_cli_client/clean/clean_system.py
+++ apps/cli-client/sharded_photos_drive_cli_client/clean/clean_system.py
@@ -16,6 +16,8 @@ from ..shared.mongodb.media_items_repository import MediaItemsRepository
 
 logger = logging.getLogger(__name__)
 
+TRASH_ALBUM_TITLE = 'To delete'
+
 
 @dataclass(frozen=True)
 class CleanupResults:
@@ -66,103 +68,122 @@ class SystemCleaner:
         self.__gphotos_clients_repo = gphotos_clients_repo
 
     def clean(self) -> CleanupResults:
-        # Step 1: Find all of the media item ids and all of the album ids to delete
-        media_item_ids_to_delete, album_ids_to_delete = (
-            self.__find_media_item_ids_and_album_ids_to_delete()
-        )
+        # Step 1: Prune all the leaf albums in the tree
+        num_albums_pruned = self.__prune_albums()
 
-        # Step 2: Find all of the GPhotos media items to move to trash
+        # Step 2: Find all content in the tree that we want to keep
+        media_item_ids, album_ids, gmedia_item_keys = self.__find_all_content()
+
+        # Step 3: Find all the media item ids to delete
+        media_item_ids_to_delete = self.__find_media_item_ids_to_delete(media_item_ids)
+
+        # Step 4: Find all album ids to delete
+        album_ids_to_delete = self.__find_album_ids_to_delete(album_ids)
+
+        # Step 5: Find all gmedia item keys to move to trash
         gmedia_item_keys_to_trash = self.__find_gmedia_item_keys_to_trash(
-            media_item_ids_to_delete
+            gmedia_item_keys
         )
 
-        # Step 3: Delete the media items that were marked to be deleted
+        # Step 6: Delete the media items that were marked to be deleted
         self.__media_items_repo.delete_many_media_items(list(media_item_ids_to_delete))
 
-        # Step 4: Delete the albums that were marked to be deleted
+        # Step 7: Delete the albums that were marked to be deleted
         self.__albums_repo.delete_many_albums(list(album_ids_to_delete))
 
-        # Step 5: Move all the gmedia items marked for trash to a folder called Trash
+        # Step 8: Move all the gmedia items marked for trash to a folder called Trash
         self.__move_gmedia_items_to_trash(list(gmedia_item_keys_to_trash))
 
         return CleanupResults(
             num_media_items_deleted=len(media_item_ids_to_delete),
-            num_albums_deleted=len(album_ids_to_delete),
+            num_albums_deleted=len(album_ids_to_delete) + num_albums_pruned,
             num_gmedia_items_moved_to_trash=len(gmedia_item_keys_to_trash),
         )
 
-    def __find_media_item_ids_and_album_ids_to_delete(
-        self,
-    ) -> tuple[set[MediaItemId], set[AlbumId]]:
-        media_item_ids_to_delete = set(
-            [item.id for item in self.__media_items_repo.get_all_media_items()]
-        )
-        album_ids_to_delete = set(
-            [item.id for item in self.__albums_repo.get_all_albums()]
-        )
-
-        albums_pruner = AlbumsPruner(
-            self.__config.get_root_album_id(), self.__albums_repo
-        )
-
+    def __prune_albums(self) -> int:
+        empty_leaf_album_ids = []
         root_album_id = self.__config.get_root_album_id()
-        album_ids_to_delete.remove(root_album_id)
         albums_queue = deque([root_album_id])
-
         while len(albums_queue) > 0:
             cur_album_id = albums_queue.popleft()
             cur_album = self.__albums_repo.get_album_by_id(cur_album_id)
 
-            if cur_album_id in album_ids_to_delete:
-                is_empty_leaf_album = (
-                    len(cur_album.child_album_ids) == 0
-                    and len(cur_album.media_item_ids) == 0
-                )
-                if not is_empty_leaf_album:
-                    album_ids_to_delete.remove(cur_album_id)
+            for child_album_id in cur_album.child_album_ids:
+                albums_queue.append(child_album_id)
+
+            if len(cur_album.child_album_ids) == 0:
+                if len(cur_album.media_item_ids) == 0:
+                    empty_leaf_album_ids.append(cur_album_id)
+
+        total_albums_pruned = 0
+        pruner = AlbumsPruner(root_album_id, self.__albums_repo)
+        for album_id in empty_leaf_album_ids:
+            total_albums_pruned += pruner.prune_album(album_id)
+
+        return total_albums_pruned
+
+    def __find_all_content(
+        self,
+    ) -> tuple[set[MediaItemId], set[AlbumId], set[GPhotosMediaItemKey]]:
+        media_item_ids = set()
+        album_ids = set()
+        gmedia_item_keys = set()
+
+        root_album_id = self.__config.get_root_album_id()
+        albums_queue = deque([root_album_id])
+        while len(albums_queue) > 0:
+            cur_album_id = albums_queue.popleft()
+            cur_album = self.__albums_repo.get_album_by_id(cur_album_id)
+            album_ids.add(cur_album_id)
 
             for child_album_id in cur_album.child_album_ids:
                 albums_queue.append(child_album_id)
 
             for media_item_id in cur_album.media_item_ids:
-                if media_item_id in media_item_ids_to_delete:
-                    media_item_ids_to_delete.remove(media_item_id)
+                media_item_ids.add(media_item_id)
 
-            if len(cur_album.child_album_ids) == 0:
-                if len(cur_album.media_item_ids) == 0:
-                    albums_pruner.prune_album(cur_album.id)
+                media_item = self.__media_items_repo.get_media_item_by_id(media_item_id)
+                gmedia_item_keys.add(
+                    GPhotosMediaItemKey(
+                        media_item.gphotos_client_id, media_item.gphotos_media_item_id
+                    )
+                )
 
-        return media_item_ids_to_delete, album_ids_to_delete
+        return media_item_ids, album_ids, gmedia_item_keys
+
+    def __find_album_ids_to_delete(self, album_ids_to_keep: set[AlbumId]):
+        all_album_ids = set([item.id for item in self.__albums_repo.get_all_albums()])
+
+        return all_album_ids - album_ids_to_keep
+
+    def __find_media_item_ids_to_delete(
+        self,
+        media_item_ids_to_keep: set[MediaItemId],
+    ) -> set[MediaItemId]:
+        all_media_item_ids = set(
+            [item.id for item in self.__media_items_repo.get_all_media_items()]
+        )
+
+        return all_media_item_ids - media_item_ids_to_keep
 
     def __find_gmedia_item_keys_to_trash(
-        self, media_item_ids_to_delete: set[MediaItemId]
+        self, gmedia_item_keys_to_keep: set[GPhotosMediaItemKey]
     ) -> set[GPhotosMediaItemKey]:
-        gmedia_item_ids_to_trash = set([])
+        all_gmedia_item_ids = set([])
         gphotos_clients = self.__gphotos_clients_repo.get_all_clients()
         for gphotos_client_id, gphotos_client in gphotos_clients:
             for gmedia_item in gphotos_client.media_items().search_for_media_items():
-                gmedia_item_key = GPhotosMediaItemKey(
-                    client_id=gphotos_client_id, object_id=gmedia_item.id
+                all_gmedia_item_ids.add(
+                    GPhotosMediaItemKey(
+                        client_id=gphotos_client_id, object_id=gmedia_item.id
+                    )
                 )
-                gmedia_item_ids_to_trash.add(gmedia_item_key)
 
-        for media_item_id in media_item_ids_to_delete:
-            media_item = self.__media_items_repo.get_media_item_by_id(media_item_id)
-            gmedia_item_key = GPhotosMediaItemKey(
-                client_id=media_item.gphotos_client_id,
-                object_id=media_item.gphotos_media_item_id,
-            )
-
-            if gmedia_item_key in gmedia_item_ids_to_trash:
-                gmedia_item_ids_to_trash.remove(gmedia_item_key)
-
-        return gmedia_item_ids_to_trash
+        return all_gmedia_item_ids - gmedia_item_keys_to_keep
 
     def __move_gmedia_items_to_trash(self, gmedia_item_keys: list[GPhotosMediaItemKey]):
         client_id_to_gmedia_item_ids: Dict[ObjectId, list[str]] = {}
-
         for key in gmedia_item_keys:
-
             if key.client_id not in client_id_to_gmedia_item_ids:
                 client_id_to_gmedia_item_ids[key.client_id] = []
 
@@ -179,12 +200,12 @@ class SystemCleaner:
     def __find_or_create_trash_album(self, client: GPhotosClientV2) -> GAlbum:
         trash_album: GAlbum | None = None
         for album in client.albums().list_albums(exclude_non_app_created_data=True):
-            if album.title == 'To delete':
+            if album.title == TRASH_ALBUM_TITLE:
                 trash_album = album
                 break
 
         if not trash_album:
-            trash_album = client.albums().create_album("To delete")
+            trash_album = client.albums().create_album(TRASH_ALBUM_TITLE)
 
         return trash_album
 
--- apps/cli-client/tests/clean/test_clean_system.py
+++ apps/cli-client/tests/clean/test_clean_system.py
@@ -1,5 +1,369 @@
 import unittest
 
+from bson import ObjectId
+
+from sharded_photos_drive_cli_client.clean.clean_system import (
+    TRASH_ALBUM_TITLE,
+    SystemCleaner,
+)
+from sharded_photos_drive_cli_client.shared.config.inmemory_config import InMemoryConfig
+from sharded_photos_drive_cli_client.shared.gphotos.clients_repository import (
+    GPhotosClientsRepository,
+)
+from sharded_photos_drive_cli_client.shared.gphotos.testing.fake_client import (
+    FakeGPhotosClient,
+)
+from sharded_photos_drive_cli_client.shared.gphotos.testing import (
+    FakeItemsRepository,
+)
+from sharded_photos_drive_cli_client.shared.mongodb.albums_repository import (
+    AlbumsRepositoryImpl,
+    UpdatedAlbumFields,
+)
+from sharded_photos_drive_cli_client.shared.mongodb.clients_repository import (
+    MongoDbClientsRepository,
+)
+from sharded_photos_drive_cli_client.shared.mongodb.media_items_repository import (
+    CreateMediaItemRequest,
+    MediaItemsRepositoryImpl,
+)
+from sharded_photos_drive_cli_client.shared.mongodb.testing.mock_mongo_client import (
+    create_mock_mongo_client,
+)
+
 
 class SystemCleanerTests(unittest.TestCase):
-    pass
+    def test_clean_deletes_unattached_albums(self):
+        # Test setup 1: Set up the config
+        mongodb_client = create_mock_mongo_client(1000)
+        gphotos_items_repo = FakeItemsRepository()
+        gphotos_client = FakeGPhotosClient(gphotos_items_repo, 'bob@gmail.com')
+        config = InMemoryConfig()
+        config.add_mongo_db_client(mongodb_client)
+        gphotos_client_id = config.add_gphotos_client(gphotos_client)
+
+        # Test setup 2: Build the wrapper objects
+        mongodb_clients_repo = MongoDbClientsRepository.build_from_config(config)
+        gphotos_clients_repo = GPhotosClientsRepository.build_from_config_repo(config)
+        albums_repo = AlbumsRepositoryImpl(mongodb_clients_repo)
+        media_items_repo = MediaItemsRepositoryImpl(mongodb_clients_repo)
+
+        # Test setup 3: Set up the root album
+        root_album = albums_repo.create_album('', None, [], [])
+        config.set_root_album_id(root_album.id)
+
+        # Test setup 4: Attach 'Archives' in root album but others not
+        archives_album = albums_repo.create_album('Archives', root_album.id, [], [])
+        albums_repo.create_album('Photos', None, [], [])
+        albums_repo.create_album('2010', None, [], [])
+        albums_repo.create_album('2011', None, [], [])
+        albums_repo.update_album(
+            root_album.id, UpdatedAlbumFields(new_child_album_ids=[archives_album.id])
+        )
+
+        # Test setup 5: Add an image to Archives
+        dog_upload_token = gphotos_client.media_items().upload_photo(
+            './Archives/dog.png', 'dog.png'
+        )
+        media_items_results = (
+            gphotos_client.media_items().add_uploaded_photos_to_gphotos(
+                [dog_upload_token]
+            )
+        )
+        dog_media_item = media_items_repo.create_media_item(
+            CreateMediaItemRequest(
+                file_name='dog.png',
+                hash_code=None,
+                location=None,
+                gphotos_client_id=ObjectId(gphotos_client_id),
+                gphotos_media_item_id=media_items_results.newMediaItemResults[
+                    0
+                ].mediaItem.id,
+            )
+        )
+        albums_repo.update_album(
+            archives_album.id,
+            UpdatedAlbumFields(new_media_item_ids=[dog_media_item.id]),
+        )
+
+        # Act: clean the system
+        cleaner = SystemCleaner(
+            config, albums_repo, media_items_repo, gphotos_clients_repo
+        )
+        clean_results = cleaner.clean()
+
+        # Assert: check on clean results
+        self.assertEqual(clean_results.num_albums_deleted, 3)
+        self.assertEqual(clean_results.num_gmedia_items_moved_to_trash, 0)
+        self.assertEqual(clean_results.num_media_items_deleted, 0)
+
+        # Assert: check that there are only 2 albums: root and Archives
+        albums = albums_repo.get_all_albums()
+        self.assertEqual(len(albums), 2)
+        self.assertEqual(albums[0].id, root_album.id)
+        self.assertEqual(albums[0].child_album_ids, [archives_album.id])
+        self.assertEqual(albums[0].media_item_ids, [])
+        self.assertEqual(albums[1].id, archives_album.id)
+        self.assertEqual(albums[1].child_album_ids, [])
+        self.assertEqual(albums[1].media_item_ids, [dog_media_item.id])
+
+        # Assert: check that there is only one photo: dog.png
+        gmedia_items = gphotos_client.media_items().search_for_media_items()
+        self.assertEqual(len(gmedia_items), 1)
+        self.assertEqual(gmedia_items[0].filename, 'dog.png')
+
+    def test_clean_with_trash_album_deletes_unattached_media_items(self):
+        # Test setup 1: Set up the config
+        mongodb_client = create_mock_mongo_client(1000)
+        gphotos_items_repo = FakeItemsRepository()
+        gphotos_client = FakeGPhotosClient(gphotos_items_repo, 'bob@gmail.com')
+        config = InMemoryConfig()
+        config.add_mongo_db_client(mongodb_client)
+        gphotos_client_id = config.add_gphotos_client(gphotos_client)
+
+        # Test setup 2: Build the wrapper objects
+        mongodb_clients_repo = MongoDbClientsRepository.build_from_config(config)
+        gphotos_clients_repo = GPhotosClientsRepository.build_from_config_repo(config)
+        albums_repo = AlbumsRepositoryImpl(mongodb_clients_repo)
+        media_items_repo = MediaItemsRepositoryImpl(mongodb_clients_repo)
+
+        # Test setup 3: Set up the root album
+        root_album = albums_repo.create_album('', None, [], [])
+        config.set_root_album_id(root_album.id)
+
+        # Test setup 4: Attach 'Archives' in root album but others not
+        archives_album = albums_repo.create_album('Archives', root_album.id, [], [])
+        albums_repo.update_album(
+            root_album.id, UpdatedAlbumFields(new_child_album_ids=[archives_album.id])
+        )
+
+        # Test setup 5: Add two images, with dog.png attached to Archives
+        dog_upload_token = gphotos_client.media_items().upload_photo(
+            './Archives/dog.png', 'dog.png'
+        )
+        dog_media_items_results = (
+            gphotos_client.media_items().add_uploaded_photos_to_gphotos(
+                [dog_upload_token]
+            )
+        )
+        dog_media_item = media_items_repo.create_media_item(
+            CreateMediaItemRequest(
+                file_name='dog.png',
+                hash_code=None,
+                location=None,
+                gphotos_client_id=ObjectId(gphotos_client_id),
+                gphotos_media_item_id=dog_media_items_results.newMediaItemResults[
+                    0
+                ].mediaItem.id,
+            )
+        )
+        albums_repo.update_album(
+            archives_album.id,
+            UpdatedAlbumFields(new_media_item_ids=[dog_media_item.id]),
+        )
+        cat_upload_token = gphotos_client.media_items().upload_photo(
+            'cat.png', 'cat.png'
+        )
+        cat_media_items_results = (
+            gphotos_client.media_items().add_uploaded_photos_to_gphotos(
+                [cat_upload_token]
+            )
+        )
+        media_items_repo.create_media_item(
+            CreateMediaItemRequest(
+                file_name='cat.png',
+                hash_code=None,
+                location=None,
+                gphotos_client_id=ObjectId(gphotos_client_id),
+                gphotos_media_item_id=cat_media_items_results.newMediaItemResults[
+                    0
+                ].mediaItem.id,
+            )
+        )
+
+        # Test setup 6: create trash album
+        trash_album = gphotos_client.albums().create_album(TRASH_ALBUM_TITLE)
+
+        # Act: clean the system
+        cleaner = SystemCleaner(
+            config, albums_repo, media_items_repo, gphotos_clients_repo
+        )
+        clean_results = cleaner.clean()
+
+        # Assert: check on clean results
+        self.assertEqual(clean_results.num_albums_deleted, 0)
+        self.assertEqual(clean_results.num_gmedia_items_moved_to_trash, 1)
+        self.assertEqual(clean_results.num_media_items_deleted, 1)
+
+        # Assert: check that there are only 2 albums: root and Archives
+        albums = albums_repo.get_all_albums()
+        self.assertEqual(len(albums), 2)
+        self.assertEqual(albums[0].id, root_album.id)
+        self.assertEqual(albums[0].child_album_ids, [archives_album.id])
+        self.assertEqual(albums[0].media_item_ids, [])
+        self.assertEqual(albums[1].id, archives_album.id)
+        self.assertEqual(albums[1].child_album_ids, [])
+        self.assertEqual(albums[1].media_item_ids, [dog_media_item.id])
+
+        # Assert: check that there is one photo in trash
+        gmedia_items = gphotos_client.media_items().search_for_media_items(
+            album_id=trash_album.id
+        )
+        self.assertEqual(len(gmedia_items), 1)
+        self.assertEqual(gmedia_items[0].filename, 'cat.png')
+
+    def test_clean_without_trash_album_deletes_unattached_media_items(self):
+        # Test setup 1: Set up the config
+        mongodb_client = create_mock_mongo_client(1000)
+        gphotos_items_repo = FakeItemsRepository()
+        gphotos_client = FakeGPhotosClient(gphotos_items_repo, 'bob@gmail.com')
+        config = InMemoryConfig()
+        config.add_mongo_db_client(mongodb_client)
+        gphotos_client_id = config.add_gphotos_client(gphotos_client)
+
+        # Test setup 2: Build the wrapper objects
+        mongodb_clients_repo = MongoDbClientsRepository.build_from_config(config)
+        gphotos_clients_repo = GPhotosClientsRepository.build_from_config_repo(config)
+        albums_repo = AlbumsRepositoryImpl(mongodb_clients_repo)
+        media_items_repo = MediaItemsRepositoryImpl(mongodb_clients_repo)
+
+        # Test setup 3: Set up the root album
+        root_album = albums_repo.create_album('', None, [], [])
+        config.set_root_album_id(root_album.id)
+
+        # Test setup 4: Attach 'Archives' in root album but others not
+        archives_album = albums_repo.create_album('Archives', root_album.id, [], [])
+        albums_repo.update_album(
+            root_album.id, UpdatedAlbumFields(new_child_album_ids=[archives_album.id])
+        )
+
+        # Test setup 5: Add two images, with dog.png attached to Archives
+        dog_upload_token = gphotos_client.media_items().upload_photo(
+            './Archives/dog.png', 'dog.png'
+        )
+        dog_media_items_results = (
+            gphotos_client.media_items().add_uploaded_photos_to_gphotos(
+                [dog_upload_token]
+            )
+        )
+        dog_media_item = media_items_repo.create_media_item(
+            CreateMediaItemRequest(
+                file_name='dog.png',
+                hash_code=None,
+                location=None,
+                gphotos_client_id=ObjectId(gphotos_client_id),
+                gphotos_media_item_id=dog_media_items_results.newMediaItemResults[
+                    0
+                ].mediaItem.id,
+            )
+        )
+        albums_repo.update_album(
+            archives_album.id,
+            UpdatedAlbumFields(new_media_item_ids=[dog_media_item.id]),
+        )
+        cat_upload_token = gphotos_client.media_items().upload_photo(
+            'cat.png', 'cat.png'
+        )
+        cat_media_items_results = (
+            gphotos_client.media_items().add_uploaded_photos_to_gphotos(
+                [cat_upload_token]
+            )
+        )
+        media_items_repo.create_media_item(
+            CreateMediaItemRequest(
+                file_name='cat.png',
+                hash_code=None,
+                location=None,
+                gphotos_client_id=ObjectId(gphotos_client_id),
+                gphotos_media_item_id=cat_media_items_results.newMediaItemResults[
+                    0
+                ].mediaItem.id,
+            )
+        )
+
+        # Act: clean the system
+        cleaner = SystemCleaner(
+            config, albums_repo, media_items_repo, gphotos_clients_repo
+        )
+        clean_results = cleaner.clean()
+
+        # Assert: check on clean results
+        self.assertEqual(clean_results.num_albums_deleted, 0)
+        self.assertEqual(clean_results.num_gmedia_items_moved_to_trash, 1)
+        self.assertEqual(clean_results.num_media_items_deleted, 1)
+
+        # Assert: check that there are only 2 albums: root and Archives
+        albums = albums_repo.get_all_albums()
+        self.assertEqual(len(albums), 2)
+        self.assertEqual(albums[0].id, root_album.id)
+        self.assertEqual(albums[0].child_album_ids, [archives_album.id])
+        self.assertEqual(albums[0].media_item_ids, [])
+        self.assertEqual(albums[1].id, archives_album.id)
+        self.assertEqual(albums[1].child_album_ids, [])
+        self.assertEqual(albums[1].media_item_ids, [dog_media_item.id])
+
+        # Assert: check that there is one photo in trash
+        trash_album = next(
+            filter(
+                lambda x: x.title == TRASH_ALBUM_TITLE,
+                gphotos_client.albums().list_albums(),
+            )
+        )
+        gmedia_items = gphotos_client.media_items().search_for_media_items(
+            album_id=trash_album.id
+        )
+        self.assertEqual(len(gmedia_items), 1)
+        self.assertEqual(gmedia_items[0].filename, 'cat.png')
+
+    def test_clean_prunes_albums(self):
+        # Test setup 1: Set up the config
+        mongodb_client = create_mock_mongo_client(1000)
+        gphotos_items_repo = FakeItemsRepository()
+        gphotos_client = FakeGPhotosClient(gphotos_items_repo, 'bob@gmail.com')
+        config = InMemoryConfig()
+        config.add_mongo_db_client(mongodb_client)
+        config.add_gphotos_client(gphotos_client)
+
+        # Test setup 2: Build the wrapper objects
+        mongodb_clients_repo = MongoDbClientsRepository.build_from_config(config)
+        gphotos_clients_repo = GPhotosClientsRepository.build_from_config_repo(config)
+        albums_repo = AlbumsRepositoryImpl(mongodb_clients_repo)
+        media_items_repo = MediaItemsRepositoryImpl(mongodb_clients_repo)
+
+        # Test setup 3: Set up the root album
+        root_album = albums_repo.create_album('', None, [], [])
+        config.set_root_album_id(root_album.id)
+        archives_album = albums_repo.create_album('Archives', root_album.id, [], [])
+        photos_album = albums_repo.create_album('Photos', archives_album.id, [], [])
+        album_2010 = albums_repo.create_album('2010', photos_album.id, [], [])
+        config.set_root_album_id(root_album.id)
+        albums_repo.update_album(
+            root_album.id,
+            UpdatedAlbumFields(new_child_album_ids=[archives_album.id]),
+        )
+        albums_repo.update_album(
+            archives_album.id,
+            UpdatedAlbumFields(new_child_album_ids=[photos_album.id]),
+        )
+        albums_repo.update_album(
+            photos_album.id,
+            UpdatedAlbumFields(new_child_album_ids=[album_2010.id]),
+        )
+
+        # Act: clean the system
+        cleaner = SystemCleaner(
+            config, albums_repo, media_items_repo, gphotos_clients_repo
+        )
+        clean_results = cleaner.clean()
+
+        # Assert: check on clean results
+        self.assertEqual(clean_results.num_albums_deleted, 3)
+        self.assertEqual(clean_results.num_gmedia_items_moved_to_trash, 0)
+        self.assertEqual(clean_results.num_media_items_deleted, 0)
+
+        # Assert: check that there is only 1 albums: root
+        albums = albums_repo.get_all_albums()
+        self.assertEqual(len(albums), 1)
+        self.assertEqual(albums[0].id, root_album.id)
+        self.assertEqual(albums[0].child_album_ids, [])
+        self.assertEqual(albums[0].media_item_ids, [])
