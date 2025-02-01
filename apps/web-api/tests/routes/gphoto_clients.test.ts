5e18ba7cfbec3f0e85e7f398bf5afb71a756ecdc
Emilio K <emiliokartono@gmail.com>
2025-02-01 10:58:22 -0800

    feat: remove deprecated apis

--- apps/web-api/tests/routes/gphoto_clients.test.ts
+++ apps/web-api/tests/routes/gphoto_clients.test.ts
@@ -4,10 +4,7 @@ import { mock } from 'jest-mock-extended';
 import { importPKCS8, SignJWT } from 'jose';
 import request from 'supertest';
 import gPhotosClientsRouter from '../../src/routes/gphoto_clients';
-import {
-  GPhotosClient,
-  GPhotosCredentials
-} from '../../src/services/blob_store/GPhotosClient';
+import { GPhotosClient } from '../../src/services/blob_store/GPhotosClient';
 import {
   GPhotosClientsRepository,
   NoGPhotosClientFoundError
@@ -45,127 +42,6 @@ describe('GPhoto Clients Router', () => {
 
   afterEach(() => {
     process.env = originalEnv;
-  });
-
-  describe('GET /api/v1/gphotos-clients', () => {
-    it('should return 200 response', async () => {
-      const client1Creds: GPhotosCredentials = {
-        token: 'client1AccessToken',
-        tokenUri: 'google.com',
-        refreshToken: 'client1RefreshToken',
-        clientId: 'gClientId',
-        clientSecret: 'gClientSecret'
-      };
-      const client2Creds: GPhotosCredentials = {
-        token: 'client2AccessToken',
-        tokenUri: 'google.com',
-        refreshToken: 'client2RefreshToken',
-        clientId: 'gClientId',
-        clientSecret: 'gClientSecret'
-      };
-      const client1 = new GPhotosClient('bob@gmail.com', client1Creds);
-      const client2 = new GPhotosClient('sam@gmail.com', client2Creds);
-      const repo = mock<GPhotosClientsRepository>();
-      repo.getGPhotosClients.mockReturnValue([
-        ['gPhotosClient1', client1],
-        ['gPhotosClient2', client2]
-      ]);
-      const app = express();
-      app.use(await gPhotosClientsRouter(repo));
-
-      const res = await request(app)
-        .get('/api/v1/gphotos-clients')
-        .set('Authorization', `Bearer ${token}`);
-
-      expect(res.statusCode).toEqual(200);
-      expect(res.body).toEqual({
-        gphotoClients: [
-          {
-            id: 'gPhotosClient1',
-            token: 'client1AccessToken'
-          },
-          {
-            id: 'gPhotosClient2',
-            token: 'client2AccessToken'
-          }
-        ]
-      });
-    });
-
-    it('should return 500 response when GPhotosClientsRepository throws an error', async () => {
-      const repo = mock<GPhotosClientsRepository>();
-      repo.getGPhotosClients.mockImplementation(() => {
-        throw new Error('Random error');
-      });
-      const app = express();
-      app.use(await gPhotosClientsRouter(repo));
-
-      const res = await request(app)
-        .get('/api/v1/gphotos-clients')
-        .set('Authorization', `Bearer ${token}`);
-
-      expect(res.statusCode).toEqual(500);
-      expect(res.body).toEqual({});
-    });
-  });
-
-  describe('POST /api/v1/gphotos-clients/:id/token-refresh', () => {
-    it('should return 200 response when token refresh is successful', async () => {
-      const client1 = mock<GPhotosClient>();
-      client1.refreshCredentials.mockResolvedValue();
-      client1.getCredentials.mockReturnValue({
-        token: 'client1AccessToken',
-        tokenUri: 'google.com',
-        refreshToken: 'client1RefreshToken',
-        clientId: 'gClientId',
-        clientSecret: 'gClientSecret'
-      });
-      const repo = mock<GPhotosClientsRepository>();
-      repo.getGPhotosClientById.mockReturnValue(client1);
-      const app = express();
-      app.use(await gPhotosClientsRouter(repo));
-
-      const res = await request(app)
-        .post('/api/v1/gphotos-clients/gPhotosClient1/token-refresh')
-        .set('Authorization', `Bearer ${token}`);
-
-      expect(res.statusCode).toEqual(200);
-      expect(res.body).toEqual({
-        newToken: 'client1AccessToken'
-      });
-    });
-
-    it('should return 404 when no client id is found', async () => {
-      const repo = mock<GPhotosClientsRepository>();
-      repo.getGPhotosClientById.mockImplementation(() => {
-        throw new NoGPhotosClientFoundError('gPhotosClient1');
-      });
-      const app = express();
-      app.use(await gPhotosClientsRouter(repo));
-
-      const res = await request(app)
-        .post('/api/v1/gphotos-clients/gPhotosClient1/token-refresh')
-        .set('Authorization', `Bearer ${token}`);
-
-      expect(res.statusCode).toEqual(404);
-      expect(res.body).toEqual({ error: 'No GPhotos client found' });
-    });
-
-    it('should return 500 when random error is thrown', async () => {
-      const repo = mock<GPhotosClientsRepository>();
-      repo.getGPhotosClientById.mockImplementation(() => {
-        throw new Error('Random error');
-      });
-      const app = express();
-      app.use(await gPhotosClientsRouter(repo));
-
-      const res = await request(app)
-        .post('/api/v1/gphotos-clients/gPhotosClient1/token-refresh')
-        .set('Authorization', `Bearer ${token}`);
-
-      expect(res.statusCode).toEqual(500);
-      expect(res.body).toEqual({});
-    });
   });
 
   describe('GET /api/v1/gphotos/:clientId/media-items/:mediaItemId', () => {
