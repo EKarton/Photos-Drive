6c6d9faf78bb7740cd6f730e875e7a695d3181a5
Emilio Kartono <18586991+EKarton@users.noreply.github.com>
2025-02-01 11:00:11 -0800

    feat: remove deprecated web apis (#50)

--- apps/web-api/src/routes/gphoto_clients.ts
+++ apps/web-api/src/routes/gphoto_clients.ts
@@ -12,48 +12,6 @@ export default async function (gPhotoClientRepo: GPhotosClientsRepository) {
   const router: Router = Router();
 
   router.get(
-    '/api/v1/gphotos-clients',
-    await verifyAuthentication(),
-    await verifyAuthorization(),
-    wrap(async (_req, res) => {
-      const results = gPhotoClientRepo.getGPhotosClients();
-      const response = {
-        gphotoClients: results.map(([id, client]) => ({
-          id: id,
-          token: client.getCredentials().token
-        }))
-      };
-
-      return res.status(200).json(response);
-    })
-  );
-
-  router.post(
-    '/api/v1/gphotos-clients/:id/token-refresh',
-    await verifyAuthentication(),
-    await verifyAuthorization(),
-    wrap(async (req, res) => {
-      const gphotosClientId = req.params.id;
-
-      try {
-        const client = gPhotoClientRepo.getGPhotosClientById(gphotosClientId);
-        await client.refreshCredentials();
-        return res.status(200).json({
-          newToken: client.getCredentials().token
-        });
-      } catch (error) {
-        if (error instanceof NoGPhotosClientFoundError) {
-          return res.status(404).json({
-            error: 'No GPhotos client found'
-          });
-        }
-
-        throw error;
-      }
-    })
-  );
-
-  router.get(
     '/api/v1/gphotos/:clientId/media-items/:mediaItemId',
     await verifyAuthentication(),
     await verifyAuthorization(),
