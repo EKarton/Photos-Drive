09533cb1b0dc01d7d6b33d7d96ced93c3507e10c
Emilio Kartono <18586991+EKarton@users.noreply.github.com>
2025-02-03 11:03:51 -0800

    feat: added video support and fixed ui (#55)

--- apps/web-ui/src/app/content-page/components/media-viewer/__tests__/media-viewer.component.spec.ts
+++ apps/web-ui/src/app/content-page/components/media-viewer/__tests__/media-viewer.component.spec.ts
@@ -14,10 +14,10 @@ import {
 } from '../../../store/media-viewer';
 import { MediaViewerComponent } from '../media-viewer.component';
 
-const MEDIA_ITEM_DETAILS: MediaItem = {
+const MEDIA_ITEM_IMAGE: MediaItem = {
   id: 'mediaItem1',
   fileName: 'dog.png',
-  hashCode: '',
+  hashCode: '1234',
   location: {
     latitude: 123,
     longitude: 456,
@@ -26,13 +26,51 @@ const MEDIA_ITEM_DETAILS: MediaItem = {
   gPhotosMediaItemId: 'gPhotosClientId1:gPhotosMediaItem1',
 };
 
-const GPHOTOS_MEDIA_ITEM_DETAILS: GPhotosMediaItem = {
+const GPHOTOS_MEDIA_ITEM_IMAGE: GPhotosMediaItem = {
   baseUrl: 'http://www.google.com/photos/1',
-  mimeType: '',
+  mimeType: 'image/png',
   mediaMetadata: {
     creationTime: '',
     width: '200',
     height: '300',
+  },
+};
+
+const MEDIA_ITEM_VIDEO: MediaItem = {
+  id: 'mediaItem2',
+  fileName: 'dog.mp4',
+  hashCode: '1234',
+  location: {
+    latitude: 123,
+    longitude: 456,
+  },
+  gPhotosClientId: 'gPhotosClientId1',
+  gPhotosMediaItemId: 'gPhotosClientId1:gPhotosMediaItem2',
+};
+
+const GPHOTOS_MEDIA_ITEM_VIDEO: GPhotosMediaItem = {
+  baseUrl: 'http://www.google.com/videos/1',
+  mimeType: 'video/mp4',
+  mediaMetadata: {
+    creationTime: '',
+    width: '200',
+    height: '300',
+  },
+};
+
+const MEDIA_ITEM_AUDIO: MediaItem = {
+  id: 'mediaItem3',
+  fileName: 'dog.mp3',
+  hashCode: '1234',
+  gPhotosClientId: 'gPhotosClientId1',
+  gPhotosMediaItemId: 'gPhotosClientId1:gPhotosMediaItem3',
+};
+
+const GPHOTOS_MEDIA_ITEM_AUDIO: GPhotosMediaItem = {
+  baseUrl: 'http://www.google.com/audio/1',
+  mimeType: 'audio/mp3',
+  mediaMetadata: {
+    creationTime: '',
   },
 };
 
@@ -104,7 +142,44 @@ describe('MediaViewerComponent', () => {
     expect(spinner).toBeTruthy();
   });
 
-  it('should render items correctly given data is already loaded', () => {
+  it('should render error message given unhandled mime type', () => {
+    store.setState({
+      [mediaViewerState.FEATURE_KEY]: {
+        request: {
+          mediaItemId: 'mediaItem3',
+        },
+        isOpen: true,
+      },
+      [mediaItemsState.FEATURE_KEY]: {
+        idToDetails: ImmutableMap().set(
+          'mediaItem3',
+          toSuccess(MEDIA_ITEM_AUDIO),
+        ),
+      },
+      [gPhotosMediaItemsState.FEATURE_KEY]: {
+        idToDetails: ImmutableMap().set(
+          'gPhotosClientId1:gPhotosMediaItem3',
+          toSuccess(GPHOTOS_MEDIA_ITEM_AUDIO),
+        ),
+      },
+    });
+    store.refreshState();
+    fixture.autoDetectChanges();
+
+    // Assert dialog is open
+    const dialog = fixture.nativeElement.querySelector('dialog');
+    expect(dialog.open).toBeTrue();
+
+    // Assert error message on content viewer is present
+    const errorMessage = fixture.nativeElement.querySelector(
+      '[data-testid="media-viewer-unknown"]',
+    );
+    expect(errorMessage.textContent!.trim()).toEqual(
+      'Unhandled media type: audio/mp3',
+    );
+  });
+
+  it('should render items correctly given loaded image data', () => {
     store.setState({
       [mediaViewerState.FEATURE_KEY]: {
         request: {
@@ -115,13 +190,13 @@ describe('MediaViewerComponent', () => {
       [mediaItemsState.FEATURE_KEY]: {
         idToDetails: ImmutableMap().set(
           'mediaItem1',
-          toSuccess(MEDIA_ITEM_DETAILS),
+          toSuccess(MEDIA_ITEM_IMAGE),
         ),
       },
       [gPhotosMediaItemsState.FEATURE_KEY]: {
         idToDetails: ImmutableMap().set(
           'gPhotosClientId1:gPhotosMediaItem1',
-          toSuccess(GPHOTOS_MEDIA_ITEM_DETAILS),
+          toSuccess(GPHOTOS_MEDIA_ITEM_IMAGE),
         ),
       },
     });
@@ -145,6 +220,9 @@ describe('MediaViewerComponent', () => {
       '[data-testid="media-viewer-new-window-button"]',
     );
     expect(newWindowButton).toBeTruthy();
+    expect(newWindowButton.getAttribute('href')).toEqual(
+      'http://www.google.com/photos/1=w200-h300',
+    );
 
     // Assert the share button is present
     const shareButton = fixture.nativeElement.querySelector(
@@ -157,6 +235,10 @@ describe('MediaViewerComponent', () => {
       '[data-testid="media-viewer-download-button"]',
     );
     expect(downloadButton).toBeTruthy();
+    expect(downloadButton.getAttribute('href')).toEqual(
+      'http://www.google.com/photos/1=d',
+    );
+    expect(downloadButton.getAttribute('download')).toEqual('dog.png');
 
     // Assert the close dialog button is present
     const closeDialogButton = fixture.nativeElement.querySelector(
@@ -179,6 +261,86 @@ describe('MediaViewerComponent', () => {
     expect(locationText.textContent!.trim()).toEqual('@ 123, 456');
   });
 
+  it('should render items correctly given loaded video data', () => {
+    store.setState({
+      [mediaViewerState.FEATURE_KEY]: {
+        request: {
+          mediaItemId: 'mediaItem2',
+        },
+        isOpen: true,
+      },
+      [mediaItemsState.FEATURE_KEY]: {
+        idToDetails: ImmutableMap().set(
+          'mediaItem2',
+          toSuccess(MEDIA_ITEM_VIDEO),
+        ),
+      },
+      [gPhotosMediaItemsState.FEATURE_KEY]: {
+        idToDetails: ImmutableMap().set(
+          'gPhotosClientId1:gPhotosMediaItem2',
+          toSuccess(GPHOTOS_MEDIA_ITEM_VIDEO),
+        ),
+      },
+    });
+    store.refreshState();
+    fixture.autoDetectChanges();
+
+    // Assert dialog is open
+    const dialog = fixture.nativeElement.querySelector('dialog');
+    expect(dialog.open).toBeTrue();
+
+    // Assert formatted text is correct
+    const formattedText = fixture.nativeElement.querySelector(
+      '[data-testid="media-viewer-formatted-date-text"]',
+    );
+    expect(formattedText.textContent!.trim()).toEqual(
+      'Sunday, November 20, 2016 at 12:35 PM',
+    );
+
+    // Assert the new window button is present
+    const newWindowButton = fixture.nativeElement.querySelector(
+      '[data-testid="media-viewer-new-window-button"]',
+    );
+    expect(newWindowButton).toBeTruthy();
+    expect(newWindowButton.getAttribute('href')).toEqual(
+      'http://www.google.com/videos/1=dv',
+    );
+
+    // Assert the share button is present
+    const shareButton = fixture.nativeElement.querySelector(
+      '[data-testid="media-viewer-share-button"]',
+    );
+    expect(shareButton).toBeTruthy();
+
+    // Assert the download button is present
+    const downloadButton = fixture.nativeElement.querySelector(
+      '[data-testid="media-viewer-download-button"]',
+    );
+    expect(downloadButton).toBeTruthy();
+    expect(downloadButton.getAttribute('href')).toEqual(
+      'http://www.google.com/videos/1=dv',
+    );
+    expect(downloadButton.getAttribute('download')).toEqual('dog.mp4');
+
+    // Assert the close dialog button is present
+    const closeDialogButton = fixture.nativeElement.querySelector(
+      '[data-testid="media-viewer-close-button"]',
+    );
+    expect(closeDialogButton).toBeTruthy();
+
+    // Assert the video is present
+    const video = fixture.nativeElement.querySelector(
+      '[data-testid="media-viewer-video"]',
+    );
+    expect(video).toBeTruthy();
+
+    // Assert the location text is present
+    const locationText = fixture.nativeElement.querySelector(
+      '[data-testid="media-viewer-location-text"]',
+    );
+    expect(locationText.textContent!.trim()).toEqual('@ 123, 456');
+  });
+
   it('should not render location when data has no location data', () => {
     store.setState({
       [mediaViewerState.FEATURE_KEY]: {
@@ -191,7 +353,7 @@ describe('MediaViewerComponent', () => {
         idToDetails: ImmutableMap().set(
           'mediaItem1',
           toSuccess({
-            ...MEDIA_ITEM_DETAILS,
+            ...MEDIA_ITEM_IMAGE,
             location: undefined,
           }),
         ),
@@ -199,7 +361,7 @@ describe('MediaViewerComponent', () => {
       [gPhotosMediaItemsState.FEATURE_KEY]: {
         idToDetails: ImmutableMap().set(
           'gPhotosClientId1:gPhotosMediaItem1',
-          toSuccess(GPHOTOS_MEDIA_ITEM_DETAILS),
+          toSuccess(GPHOTOS_MEDIA_ITEM_IMAGE),
         ),
       },
     });
@@ -213,7 +375,7 @@ describe('MediaViewerComponent', () => {
     expect(locationText).toBeFalsy();
   });
 
-  it('should call navigator api correctly when user clicks on share button', () => {
+  it('should call navigator api correctly when user clicks on share button on an image', () => {
     mockNavigator.canShare.and.returnValue(true);
     mockNavigator.share.and.resolveTo(undefined);
     store.setState({
@@ -226,13 +388,13 @@ describe('MediaViewerComponent', () => {
       [mediaItemsState.FEATURE_KEY]: {
         idToDetails: ImmutableMap().set(
           'mediaItem1',
-          toSuccess(MEDIA_ITEM_DETAILS),
+          toSuccess(MEDIA_ITEM_IMAGE),
         ),
       },
       [gPhotosMediaItemsState.FEATURE_KEY]: {
         idToDetails: ImmutableMap().set(
           'gPhotosClientId1:gPhotosMediaItem1',
-          toSuccess(GPHOTOS_MEDIA_ITEM_DETAILS),
+          toSuccess(GPHOTOS_MEDIA_ITEM_IMAGE),
         ),
       },
     });
@@ -251,6 +413,44 @@ describe('MediaViewerComponent', () => {
     });
   });
 
+  it('should call navigator api correctly when user clicks on share button on a video', () => {
+    mockNavigator.canShare.and.returnValue(true);
+    mockNavigator.share.and.resolveTo(undefined);
+    store.setState({
+      [mediaViewerState.FEATURE_KEY]: {
+        request: {
+          mediaItemId: 'mediaItem2',
+        },
+        isOpen: true,
+      },
+      [mediaItemsState.FEATURE_KEY]: {
+        idToDetails: ImmutableMap().set(
+          'mediaItem2',
+          toSuccess(MEDIA_ITEM_VIDEO),
+        ),
+      },
+      [gPhotosMediaItemsState.FEATURE_KEY]: {
+        idToDetails: ImmutableMap().set(
+          'gPhotosClientId1:gPhotosMediaItem2',
+          toSuccess(GPHOTOS_MEDIA_ITEM_VIDEO),
+        ),
+      },
+    });
+    store.refreshState();
+    fixture.autoDetectChanges();
+
+    const shareButton = fixture.nativeElement.querySelector(
+      '[data-testid="media-viewer-share-button"]',
+    );
+    shareButton.click();
+
+    expect(mockNavigator.share).toHaveBeenCalledWith({
+      title: 'dog.mp4',
+      text: 'Photo from Sharded Photos Drive',
+      url: 'http://www.google.com/videos/1=dv',
+    });
+  });
+
   it('should not call navigator.share() when user clicks on share button but sharing fails', () => {
     mockNavigator.canShare.and.returnValue(false);
     mockNavigator.share.and.resolveTo(undefined);
@@ -264,13 +464,13 @@ describe('MediaViewerComponent', () => {
       [mediaItemsState.FEATURE_KEY]: {
         idToDetails: ImmutableMap().set(
           'mediaItem1',
-          toSuccess(MEDIA_ITEM_DETAILS),
+          toSuccess(MEDIA_ITEM_IMAGE),
         ),
       },
       [gPhotosMediaItemsState.FEATURE_KEY]: {
         idToDetails: ImmutableMap().set(
           'gPhotosClientId1:gPhotosMediaItem1',
-          toSuccess(GPHOTOS_MEDIA_ITEM_DETAILS),
+          toSuccess(GPHOTOS_MEDIA_ITEM_IMAGE),
         ),
       },
     });
@@ -296,13 +496,13 @@ describe('MediaViewerComponent', () => {
       [mediaItemsState.FEATURE_KEY]: {
         idToDetails: ImmutableMap().set(
           'mediaItem1',
-          toSuccess(MEDIA_ITEM_DETAILS),
+          toSuccess(MEDIA_ITEM_IMAGE),
         ),
       },
       [gPhotosMediaItemsState.FEATURE_KEY]: {
         idToDetails: ImmutableMap().set(
           'gPhotosClientId1:gPhotosMediaItem1',
-          toSuccess(GPHOTOS_MEDIA_ITEM_DETAILS),
+          toSuccess(GPHOTOS_MEDIA_ITEM_IMAGE),
         ),
       },
     });
--- apps/web-ui/src/app/content-page/components/media-viewer/media-viewer.component.ts
+++ apps/web-ui/src/app/content-page/components/media-viewer/media-viewer.component.ts
@@ -25,7 +25,9 @@ import { mediaViewerActions, mediaViewerState } from '../../store/media-viewer';
 
 /** The details to display to the UI. */
 interface MediaDetails {
-  imageUrl: string;
+  url: string;
+  downloadUrl: string;
+  mimeType: string;
   imageAlt: string;
   fileName: string;
   formattedDate: string;
@@ -81,7 +83,9 @@ export class MediaViewerComponent implements AfterViewInit, OnDestroy {
           mediaItemResult,
           gPhotosMediaItemResult,
           (mediaItem, gPhotosMediaItem) => ({
-            imageUrl: `${gPhotosMediaItem.baseUrl}=w${gPhotosMediaItem.mediaMetadata.width}-h${gPhotosMediaItem.mediaMetadata.height}`,
+            url: getUrl(gPhotosMediaItem),
+            downloadUrl: getDownloadUrl(gPhotosMediaItem),
+            mimeType: gPhotosMediaItem.mimeType,
             imageAlt: `Image of ${mediaItem.fileName}`,
             fileName: mediaItem.fileName,
             formattedDate: 'Sunday, November 20, 2016 at 12:35 PM',
@@ -134,4 +138,20 @@ export class MediaViewerComponent implements AfterViewInit, OnDestroy {
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
+}
+
+function getUrl(gMediaItem: GPhotosMediaItem): string {
+  if (gMediaItem.mimeType.startsWith('image')) {
+    return `${gMediaItem.baseUrl}=w${gMediaItem.mediaMetadata.width}-h${gMediaItem.mediaMetadata.height}`;
+  }
+
+  return `${gMediaItem.baseUrl}=dv`;
+}
+
+function getDownloadUrl(gMediaItem: GPhotosMediaItem): string {
+  if (gMediaItem.mimeType.startsWith('image')) {
+    return `${gMediaItem.baseUrl}=d`;
+  }
+
+  return `${gMediaItem.baseUrl}=dv`;
 }
