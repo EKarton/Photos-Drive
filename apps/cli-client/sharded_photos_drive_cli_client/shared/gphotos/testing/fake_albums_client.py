fff2a23bb0fe1e74309d8e6d8de73733619e9cde
Emilio K <emiliokartono@gmail.com>
2025-02-08 19:44:41 -0800

    wip

--- apps/cli-client/sharded_photos_drive_cli_client/shared/gphotos/testing/fake_albums_client.py
+++ apps/cli-client/sharded_photos_drive_cli_client/shared/gphotos/testing/fake_albums_client.py
@@ -30,7 +30,7 @@ class FakeGPhotosAlbumsClient(GPhotosAlbumsClient):
 
     def add_photos_to_album(self, album_id: str, media_item_ids: list[str]):
         if len(media_item_ids) > 50:
-            raise Exception("Must have less than 50 media item ids")
+            raise ValueError("Must have less than 50 media item ids")
         self.repository.add_photos_to_album(self.id, album_id, media_item_ids)
 
     def remove_photos_from_album(self, album_id: str, media_item_ids: list[str]):
