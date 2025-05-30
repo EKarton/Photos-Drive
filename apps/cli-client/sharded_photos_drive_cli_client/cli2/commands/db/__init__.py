5f9d5183d9946cd9ee3f5530a2932a49a45ada10
Emilio Kartono <18586991+EKarton@users.noreply.github.com>
2025-05-30 11:25:59 -0700

    feat: create script to delete Albums.media _item_ids from db (#82)

--- apps/cli-client/sharded_photos_drive_cli_client/cli2/commands/db/__init__.py
+++ apps/cli-client/sharded_photos_drive_cli_client/cli2/commands/db/__init__.py
@@ -1,5 +1,6 @@
 import typer
 
+from .delete_media_item_ids import delete_media_item_ids
 from .dump import dump
 from .restore import restore
 
@@ -7,3 +8,4 @@ from .restore import restore
 app = typer.Typer()
 app.command()(dump)
 app.command()(restore)
+app.command()(delete_media_item_ids)
