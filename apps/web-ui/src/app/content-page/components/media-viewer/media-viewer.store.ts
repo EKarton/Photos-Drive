9af6145f84294fb5f0a95e320bdc50a0f0602adb
Emilio K <emiliokartono@gmail.com>
2025-06-01 18:14:59 -0700

    wip

---
+++ apps/web-ui/src/app/content-page/components/media-viewer/media-viewer.store.ts
@@ -0,0 +1,102 @@
+import { inject } from '@angular/core';
+import { ComponentStore } from '@ngrx/component-store';
+import { Store } from '@ngrx/store';
+import { switchMap, tap } from 'rxjs/operators';
+
+import { authState } from '../../../auth/store';
+import { Result, toPending } from '../../../shared/results/results';
+import { switchMapResultToResultRxJs } from '../../../shared/results/rxjs/switchMapResultToResultRxJs';
+import { toResult } from '../../../shared/results/rxjs/toResult';
+import {
+  GPhotosMediaItem,
+  GPhotosMediaItemDetailsApiResponse,
+  MediaItem,
+  MediaItemDetailsApiResponse,
+  WebApiService,
+} from '../../services/webapi.service';
+
+export interface MediaViewerState {
+  mediaItemResult: Result<MediaItem>;
+  gMediaItemResult: Result<GPhotosMediaItem>;
+}
+
+export const INITIAL_STATE: MediaViewerState = {
+  mediaItemResult: toPending(),
+  gMediaItemResult: toPending(),
+};
+
+export class MediaViewerStore extends ComponentStore<MediaViewerState> {
+  private readonly store = inject(Store);
+  private readonly webApiService = inject(WebApiService);
+
+  constructor() {
+    super(INITIAL_STATE);
+  }
+
+  readonly mediaItemResult = this.selectSignal(
+    (state) => state.mediaItemResult,
+  );
+  readonly gMediaItemResult = this.selectSignal(
+    (state) => state.gMediaItemResult,
+  );
+
+  private readonly clearStates = this.updater(
+    (): MediaViewerState => ({
+      ...INITIAL_STATE,
+    }),
+  );
+
+  private readonly setMediaItemResult = this.updater(
+    (
+      state: MediaViewerState,
+      response: Result<MediaItemDetailsApiResponse>,
+    ): MediaViewerState => ({
+      ...state,
+      mediaItemResult: response,
+    }),
+  );
+
+  private readonly setGMediaItemResult = this.updater(
+    (
+      state: MediaViewerState,
+      response: Result<GPhotosMediaItemDetailsApiResponse>,
+    ): MediaViewerState => ({
+      ...state,
+      gMediaItemResult: response,
+    }),
+  );
+
+  readonly loadDetails = this.effect<string>((mediaItemId$) =>
+    mediaItemId$.pipe(
+      switchMap((mediaItemId) => {
+        this.clearStates();
+
+        return this.store.select(authState.selectAuthToken).pipe(
+          switchMap((accessToken) => {
+            return this.webApiService
+              .fetchMediaItemDetails(accessToken, mediaItemId)
+              .pipe(
+                toResult<MediaItemDetailsApiResponse>(),
+                tap((mediaItemResult) =>
+                  this.setMediaItemResult(mediaItemResult),
+                ),
+                switchMapResultToResultRxJs((mediaItem) => {
+                  return this.webApiService
+                    .fetchGPhotosMediaItemDetails(
+                      accessToken,
+                      mediaItem.gPhotosMediaItemId,
+                    )
+                    .pipe(
+                      toResult<GPhotosMediaItemDetailsApiResponse>(),
+                      tap((gMediaItemResult) =>
+                        this.setGMediaItemResult(gMediaItemResult),
+                      ),
+                    );
+                }),
+              );
+          }),
+        );
+      }),
+    ),
+  );
+}
