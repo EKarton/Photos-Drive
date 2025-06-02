f5c2e3265c958739bce8f805ae5ca41c317edbbc
Emilio Kartono <18586991+EKarton@users.noreply.github.com>
2025-06-02 01:05:48 -0700

    feat: add http caching in web ui (#88)

---
+++ apps/web-ui/src/app/content-page/interceptors/__tests__/webapi-cache.interceptor.spec.ts
@@ -0,0 +1,98 @@
+import {
+  HttpClient,
+  HttpResponse,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
+import {
+  HttpTestingController,
+  provideHttpClientTesting,
+} from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+
+import { environment } from '../../../../environments/environment';
+import { HttpCacheService } from '../http-cache.service';
+import { webApiHttpCacheInterceptor } from '../webapi-cache.interceptor';
+
+describe('webApiHttpCacheInterceptor', () => {
+  let mockCacheService: jasmine.SpyObj<HttpCacheService>;
+  let httpClient: HttpClient;
+  let httpMock: HttpTestingController;
+  let testUrl: string;
+
+  beforeEach(() => {
+    mockCacheService = jasmine.createSpyObj<HttpCacheService>(
+      'HttpCacheService',
+      ['get', 'set'],
+    );
+    environment.webApiEndpoint = 'http://localhost:3000';
+    testUrl = `${environment.webApiEndpoint}/api/v1/albums/123/media-items`;
+
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(withInterceptors([webApiHttpCacheInterceptor])),
+        provideHttpClientTesting(),
+        { provide: HttpCacheService, useValue: mockCacheService },
+      ],
+    });
+
+    httpClient = TestBed.inject(HttpClient);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should return cached response if available', (done) => {
+    mockCacheService.get.and.returnValue(
+      new HttpResponse({
+        body: 'My data',
+        status: 200,
+      }),
+    );
+
+    httpClient.get(testUrl).subscribe((res) => {
+      expect(res).toEqual('My data');
+      expect(mockCacheService.get).toHaveBeenCalledWith(testUrl);
+      done();
+    });
+  });
+
+  it('should call next() and cache the response if not cached', (done) => {
+    mockCacheService.get.and.returnValue(undefined);
+
+    httpClient.get(testUrl).subscribe((res) => {
+      expect(res).toEqual('My data');
+      expect(mockCacheService.get).toHaveBeenCalledWith(testUrl);
+      expect(mockCacheService.set).toHaveBeenCalledWith(
+        testUrl,
+        jasmine.any(HttpResponse),
+        60 * 60 * 1000,
+      );
+      done();
+    });
+    httpMock.expectOne(testUrl).flush('My data');
+  });
+
+  it('should skip caching for non-GET requests', (done) => {
+    httpClient.post(testUrl, {}).subscribe((res) => {
+      expect(res).toEqual('My data');
+      expect(mockCacheService.get).not.toHaveBeenCalled();
+      expect(mockCacheService.set).not.toHaveBeenCalled();
+      done();
+    });
+    httpMock.expectOne(testUrl).flush('My data');
+  });
+
+  it('should skip caching for URLs outside webApiEndpoint', (done) => {
+    const testUrl = 'https://other.com/api/data';
+    httpClient.get(testUrl).subscribe((res) => {
+      expect(res).toEqual('My data');
+      expect(mockCacheService.get).not.toHaveBeenCalled();
+      expect(mockCacheService.set).not.toHaveBeenCalled();
+      done();
+    });
+    httpMock.expectOne(testUrl).flush('My data');
+  });
+});
