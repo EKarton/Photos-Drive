5a1a87586a8feaa5ae66a3e5bebe617ba1f7c1b0
Emilio Kartono <18586991+EKarton@users.noreply.github.com>
2025-06-06 18:17:30 -0700

    feat: create script to populate width, height, and date_taken in media_items db (#92)

--- apps/cli-client/sharded_photos_drive_cli_client/cli2/commands/db/__init__.py
+++ apps/cli-client/sharded_photos_drive_cli_client/cli2/commands/db/__init__.py
@@ -1,5 +1,8 @@
 import typer
 
+from .set_media_item_width_height_date_taken_fields import (
+    set_media_item_width_height_date_taken_fields,
+)
 from .delete_media_item_ids import delete_media_item_ids
 from .dump import dump
 from .restore import restore
@@ -9,3 +12,4 @@ app = typer.Typer()
 app.command()(dump)
 app.command()(restore)
 app.command()(delete_media_item_ids)
+app.command()(set_media_item_width_height_date_taken_fields)
