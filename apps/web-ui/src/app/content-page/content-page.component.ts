86b20be63353b9e87447ea9c2b375c559ec13b04
Emilio Kartono <18586991+EKarton@users.noreply.github.com>
2025-07-05 16:45:44 -0700

    chore: move albums list to albums view component (#137)

--- apps/web-ui/src/app/content-page/content-page.component.ts
+++ apps/web-ui/src/app/content-page/content-page.component.ts
@@ -1,19 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Store } from '@ngrx/store';
-import { map, Observable, Subscription, switchMap } from 'rxjs';
+import { Component } from '@angular/core';
 
-import { HasFailedPipe } from '../shared/results/pipes/has-failed.pipe';
-import { IsPendingPipe } from '../shared/results/pipes/is-pending.pipe';
-import { Result } from '../shared/results/results';
-import { AlbumsListComponent } from './components/albums-list/albums-list.component';
-import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
-import { HeaderComponent } from './components/header/header.component';
-import { ImagesSectionComponent } from './components/images-section/images-section.component';
-import { MediaViewerComponent } from './components/media-viewer/media-viewer.component';
-import { Album } from './services/types/album';
-import { albumsActions, albumsState } from './store/albums';
+import { AlbumsViewComponent } from './albums-view/albums-view.component';
+import { HeaderComponent } from './albums-view/header/header.component';
+import { MediaViewerComponent } from './albums-view/media-viewer/media-viewer.component';
 
 @Component({
   standalone: true,
@@ -21,39 +11,9 @@ import { albumsActions, albumsState } from './store/albums';
   imports: [
     CommonModule,
     HeaderComponent,
-    IsPendingPipe,
-    HasFailedPipe,
-    AlbumsListComponent,
-    ImagesSectionComponent,
-    BreadcrumbsComponent,
+    AlbumsViewComponent,
     MediaViewerComponent,
   ],
   templateUrl: './content-page.component.html',
 })
-export class ContentPageComponent implements OnInit, OnDestroy {
-  private readonly store = inject(Store);
-  private readonly route = inject(ActivatedRoute);
-  private readonly subscription = new Subscription();
-
-  readonly albumId$ = this.route.paramMap.pipe(
-    map((params) => params.get('albumId')!),
-  );
-
-  readonly albumResult$: Observable<Result<Album>> = this.albumId$.pipe(
-    switchMap((albumId) =>
-      this.store.select(albumsState.selectAlbumDetailsById(albumId)),
-    ),
-  );
-
-  ngOnInit() {
-    this.subscription.add(
-      this.albumId$.subscribe((albumId) => {
-        this.store.dispatch(albumsActions.loadAlbumDetails({ albumId }));
-      }),
-    );
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
-}
+export class ContentPageComponent {}
