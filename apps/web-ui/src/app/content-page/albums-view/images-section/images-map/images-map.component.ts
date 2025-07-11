2d3477ca697c1cfb2017ed4baa3e106471f00b64
Emilio K <emiliokartono@gmail.com>
2025-07-11 00:56:30 -0700

    wip

--- apps/web-ui/src/app/content-page/albums-view/images-section/images-map/images-map.component.ts
+++ apps/web-ui/src/app/content-page/albums-view/images-section/images-map/images-map.component.ts
@@ -1,11 +1,16 @@
 import { CommonModule } from '@angular/common';
 import {
+  AfterViewInit,
   Component,
   computed,
   effect,
+  ElementRef,
+  HostListener,
   inject,
   input,
   Signal,
+  signal,
+  ViewChild,
 } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Store } from '@ngrx/store';
@@ -23,11 +28,14 @@ import { ImagesMapViewerComponent } from './images-map-viewer/images-map-viewer.
   templateUrl: './images-map.component.html',
   providers: [ImagesMapStore],
 })
-export class ImagesMapComponent {
+export class ImagesMapComponent implements AfterViewInit {
   readonly albumId = input.required<string>();
 
   private readonly store = inject(Store);
   private readonly imagesMapViewStore = inject(ImagesMapStore);
+
+  @ViewChild('fullscreenContainer', { static: true })
+  fullscreenContainer!: ElementRef;
 
   readonly isFetchingImages: Signal<boolean> =
     this.imagesMapViewStore.isFetchingImages;
@@ -49,5 +57,50 @@ export class ImagesMapComponent {
         albumId: this.albumId(),
       });
     });
+  }
+
+  @HostListener('document:keydown.escape', ['$event'])
+  onEscapeKey() {
+    console.log('Escape pressed');
+    if (document.fullscreenElement) {
+      document.exitFullscreen();
+    }
+  }
+
+  readonly isFullscreen = signal(false);
+
+  ngAfterViewInit() {
+    console.log('View initialized:', this.fullscreenContainer);
+  }
+
+  toggleFullscreen() {
+    if (!this.isFullscreen()) {
+      this.goFullscreen();
+      this.isFullscreen.set(true);
+    } else {
+      this.exitFullscreen();
+      this.isFullscreen.set(false);
+    }
+  }
+
+  goFullscreen() {
+    const elem = this.fullscreenContainer.nativeElement;
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if ((elem as any).webkitRequestFullscreen) {
+      (elem as any).webkitRequestFullscreen(); // Safari
+    } else if ((elem as any).msRequestFullscreen) {
+      (elem as any).msRequestFullscreen(); // IE/Edge
+    }
+  }
+
+  exitFullscreen() {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if ((document as any).webkitExitFullscreen) {
+      (document as any).webkitExitFullscreen();
+    } else if ((document as any).msExitFullscreen) {
+      (document as any).msExitFullscreen();
+    }
   }
 }
