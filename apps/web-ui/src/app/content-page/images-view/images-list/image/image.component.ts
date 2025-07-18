7418d1e1268fc1e53dbff608cb1ce3379ac38410
Emilio Kartono <18586991+EKarton@users.noreply.github.com>
2025-07-18 10:31:31 -0700

    feat: add images view to photos tab (#159)

---
+++ apps/web-ui/src/app/content-page/images-view/images-list/image/image.component.ts
@@ -0,0 +1,99 @@
+import {
+  Component,
+  computed,
+  effect,
+  inject,
+  input,
+  signal,
+} from '@angular/core';
+import { Store } from '@ngrx/store';
+import { InViewportModule } from 'ng-in-viewport';
+
+import { WINDOW } from '../../../../app.tokens';
+import { HasFailedPipe } from '../../../../shared/results/pipes/has-failed.pipe';
+import { IsPendingPipe } from '../../../../shared/results/pipes/is-pending.pipe';
+import { mapResult } from '../../../../shared/results/utils/mapResult';
+import { GPhotosMediaItem } from '../../../services/types/gphotos-media-item';
+import { mediaViewerActions } from '../../../store/media-viewer';
+import { ImageStore } from './image.store';
+
+export interface ImageData {
+  id: string;
+  baseUrl: string;
+  fileName: string;
+  onClick: (event: MouseEvent) => void;
+  onKeyDown: (event: KeyboardEvent) => void;
+}
+
+@Component({
+  selector: 'app-image',
+  imports: [InViewportModule, HasFailedPipe, IsPendingPipe],
+  templateUrl: './image.component.html',
+  providers: [ImageStore],
+})
+export class ImageComponent {
+  readonly mediaItemId = input.required<string>();
+  readonly gPhotosMediaItemId = input.required<string>();
+  readonly fileName = input.required<string>();
+  readonly width = input.required<number>();
+  readonly height = input.required<number>();
+
+  private readonly store = inject(Store);
+  private readonly imageStore = inject(ImageStore);
+  private readonly window = inject(WINDOW);
+
+  private readonly isInViewport = signal(false);
+
+  readonly imageDataResult = computed(() => {
+    const gMediaItemResult = this.imageStore.gPhotosMediaItem();
+
+    return mapResult(gMediaItemResult, (gMediaItem) => {
+      return {
+        baseUrl: gMediaItem.baseUrl!,
+        onClick: (event: MouseEvent) => {
+          if (event.ctrlKey) {
+            this.openImageInNewTab(gMediaItem);
+          } else {
+            this.openImageInDialog(this.mediaItemId());
+          }
+        },
+        onKeyDown: (event: KeyboardEvent) => {
+          if (event.ctrlKey && event.key === 'Enter') {
+            event.preventDefault();
+            this.openImageInNewTab(gMediaItem);
+          } else if (event.key === 'Enter') {
+            event.preventDefault();
+            this.openImageInDialog(this.mediaItemId());
+          }
+        },
+      };
+    });
+  });
+
+  constructor() {
+    effect(() => {
+      if (this.isInViewport()) {
+        this.imageStore.loadGPhotosMediaItemDetails(this.gPhotosMediaItemId());
+      }
+    });
+  }
+
+  setIsInViewport(visible: boolean) {
+    this.isInViewport.set(visible);
+  }
+
+  private openImageInNewTab(detail: GPhotosMediaItem) {
+    const width = detail.mediaMetadata.width;
+    const height = detail.mediaMetadata.height;
+    const fullPageUrl = `${detail.baseUrl}=w${width}-h${height}`;
+    this.window.open(fullPageUrl, '_blank');
+  }
+
+  private openImageInDialog(mediaItemId: string) {
+    this.store.dispatch(
+      mediaViewerActions.openMediaViewer({
+        request: { mediaItemId },
+      }),
+    );
+  }
+}
---
+++ apps/web-ui/src/app/content-page/media-viewer/media-viewer.store.ts
@@ -0,0 +1,101 @@
+import { inject, Injectable } from '@angular/core';
+import { ComponentStore } from '@ngrx/component-store';
+import { Store } from '@ngrx/store';
+import { switchMap, tap } from 'rxjs/operators';
+
+import { authState } from '../../auth/store';
+import { Result, toPending } from '../../shared/results/results';
+import { switchMapResultToResultRxJs } from '../../shared/results/rxjs/switchMapResultToResultRxJs';
+import { GPhotosMediaItemDetailsApiResponse } from '../services/types/gphotos-media-item';
+import { GPhotosMediaItem } from '../services/types/gphotos-media-item';
+import { MediaItemDetailsApiResponse } from '../services/types/media-item';
+import { MediaItem } from '../services/types/media-item';
+import { WebApiService } from '../services/webapi.service';
+
+/** The state definition for {@code MediaViewerStore} */
+export interface MediaViewerState {
+  mediaItemResult: Result<MediaItem>;
+  gMediaItemResult: Result<GPhotosMediaItem>;
+}
+
+/** The initial state for the {@code MediaViewerStore} */
+export const INITIAL_STATE: MediaViewerState = {
+  mediaItemResult: toPending(),
+  gMediaItemResult: toPending(),
+};
+
+/** A component store for the {@code MediaViewerComponent} */
+@Injectable()
+export class MediaViewerStore extends ComponentStore<MediaViewerState> {
+  private readonly store = inject(Store);
+  private readonly webApiService = inject(WebApiService);
+
+  constructor() {
+    super(INITIAL_STATE);
+  }
+
+  readonly mediaItemResult = this.selectSignal(
+    (state) => state.mediaItemResult,
+  );
+  readonly gMediaItemResult = this.selectSignal(
+    (state) => state.gMediaItemResult,
+  );
+
+  private readonly clearStates = this.updater(
+    (): MediaViewerState => ({
+      ...INITIAL_STATE,
+    }),
+  );
+
+  private readonly setMediaItemResult = this.updater(
+    (
+      state: MediaViewerState,
+      response: Result<MediaItemDetailsApiResponse>,
+    ): MediaViewerState => ({
+      ...state,
+      mediaItemResult: response,
+    }),
+  );
+
+  private readonly setGMediaItemResult = this.updater(
+    (
+      state: MediaViewerState,
+      response: Result<GPhotosMediaItemDetailsApiResponse>,
+    ): MediaViewerState => ({
+      ...state,
+      gMediaItemResult: response,
+    }),
+  );
+
+  readonly loadDetails = this.effect<string>((mediaItemId$) =>
+    mediaItemId$.pipe(
+      switchMap((mediaItemId) => {
+        this.clearStates();
+
+        return this.store.select(authState.selectAuthToken).pipe(
+          switchMap((accessToken) => {
+            return this.webApiService
+              .getMediaItem(accessToken, mediaItemId)
+              .pipe(
+                tap((mediaItemResult) =>
+                  this.setMediaItemResult(mediaItemResult),
+                ),
+                switchMapResultToResultRxJs((mediaItem) => {
+                  return this.webApiService
+                    .getGPhotosMediaItem(
+                      accessToken,
+                      mediaItem.gPhotosMediaItemId,
+                    )
+                    .pipe(
+                      tap((gMediaItemResult) =>
+                        this.setGMediaItemResult(gMediaItemResult),
+                      ),
+                    );
+                }),
+              );
+          }),
+        );
+      }),
+    ),
+  );
+}
