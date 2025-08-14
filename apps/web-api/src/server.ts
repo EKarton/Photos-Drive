609fe7109dc4934c8eda27296c93dcae6e746246
Emilio Kartono <18586991+EKarton@users.noreply.github.com>
2025-08-14 16:35:53 -0700

    fix: pre-download ml model before use (#174)

--- apps/web-api/src/server.ts
+++ apps/web-api/src/server.ts
@@ -3,42 +3,54 @@ import OS from 'os';
 import Process from 'process';
 import dotenv from 'dotenv';
 import { App } from './app';
+import { OpenCLIPImageEmbedder } from './services/ml/models/OpenCLIPImageEmbeddings';
 import logger from './utils/logger';
 
-let numRetries = 10;
+let numRetries = 3;
 
-dotenv.config();
+async function main() {
+  dotenv.config();
 
-if (Cluster.isPrimary) {
-  const numForks = Number(process.env.NUM_FORKS) || OS.cpus().length;
+  if (Cluster.isPrimary) {
+    // Pre-download the models before it gets used in the sub-processes
+    const mlModelToPreload = new OpenCLIPImageEmbedder();
+    await mlModelToPreload.initialize();
 
-  for (let i = 0; i < numForks; i++) {
-    Cluster.fork();
-  }
+    const numForks = Number(process.env.NUM_FORKS) || OS.cpus().length;
 
-  // Fork the server again if it dies
-  Cluster.on('exit', (_worker) => {
-    logger.info('A worker has died!');
-    numRetries--;
-
-    if (numRetries > 0) {
-      logger.info('Relaunching worker again');
+    for (let i = 0; i < numForks; i++) {
       Cluster.fork();
-    } else {
-      logger.info('Not launching worker again');
     }
-  });
-} else {
-  logger.info(`Child process #${Process.pid} spawned`);
 
-  const app = new App();
-  app.run();
+    // Fork the server again if it dies
+    Cluster.on('exit', (_worker) => {
+      logger.info('A worker has died!');
+      numRetries--;
 
-  process.on('SIGINT', async () => {
-    await app.shutdown();
-  });
+      if (numRetries > 0) {
+        logger.info('Relaunching worker again');
+        Cluster.fork();
+      } else {
+        logger.info('Not launching worker again');
+      }
+    });
+  } else {
+    logger.info(`Child process #${Process.pid} spawned`);
 
-  process.on('exit', async () => {
-    await app.shutdown();
-  });
+    const app = new App();
+    app.run();
+
+    process.on('SIGINT', async () => {
+      await app.shutdown();
+    });
+
+    process.on('exit', async () => {
+      await app.shutdown();
+    });
+  }
 }
+
+main().catch((err) => {
+  console.error('Fatal error in main:', err);
+  process.exit(1);
+});
