94e10a7997ae7dd03737e19df64eda3710ac3039
Emilio K <emiliokartono@gmail.com>
2025-09-28 19:21:16 -0700

    fix: improve landing page

--- apps/web-ui/src/app/home-page/home-page.component.ts
+++ apps/web-ui/src/app/home-page/home-page.component.ts
@@ -1,16 +1,25 @@
-import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener, inject } from '@angular/core';
 
 import { environment } from '../../environments/environment';
 import { WINDOW } from '../app.tokens';
+import { ThemeToggleButtonComponent } from '../themes/components/theme-toggle-button/theme-toggle-button.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [CommonModule, ThemeToggleButtonComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
 export class HomePageComponent {
   private readonly window: Window = inject(WINDOW);
+
+  scrolled = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.scrolled = window.scrollY > 50; // toggle when scrolled more than 50px
+  }
 
   handleLoginClick() {
     this.window.location.href = `${environment.loginUrl}?select_account=true`;
