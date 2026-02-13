69297104727cef9d1595785cba1dcf743698b4cb
Emilio K <e.kartonoe@gmail.com>
2026-02-12 23:10:55 -0800

    wip

--- apps/cli-client/tests/shared/core/albums/repository/test_mongodb.py
+++ apps/cli-client/tests/shared/core/albums/repository/test_mongodb.py
@@ -33,7 +33,6 @@ class TestAlbumsRepositoryImpl(unittest.TestCase):
         )
 
     def test_get_album_by_id(self):
-        # Prepare test data
         album_id = AlbumId(MONGO_CLIENT_ID, ObjectId("5f50c31e8a7d4b1c9c9b0b1b"))
         self.mock_client["photos_drive"]["albums"].insert_one(
             {
@@ -43,10 +42,8 @@ class TestAlbumsRepositoryImpl(unittest.TestCase):
             }
         )
 
-        # Test
         album = self.repo.get_album_by_id(album_id)
 
-        # Assert
         self.assertEqual(album.id, album_id)
         self.assertEqual(album.name, "Test Album")
         self.assertEqual(
@@ -57,8 +54,15 @@ class TestAlbumsRepositoryImpl(unittest.TestCase):
             ),
         )
 
+    def test_get_album_by_id_wrong_client_id(self):
+        with self.assertRaisesRegex(
+            ValueError, "Album .* belongs to a different client"
+        ):
+            self.repo.get_album_by_id(
+                AlbumId(ObjectId(), ObjectId("5f50c31e8a7d4b1c9c9b0b1b"))
+            )
+
     def test_get_all_albums(self):
-        # Arrange: Prepare test data
         album_id_1 = AlbumId(MONGO_CLIENT_ID, ObjectId("5f50c31e8a7d4b1c9c9b0b1b"))
         album_id_2 = AlbumId(MONGO_CLIENT_ID, ObjectId("5f50c31e8a7d4b1c9c9b0b1c"))
         self.mock_client["photos_drive"]["albums"].insert_one(
@@ -76,15 +80,12 @@ class TestAlbumsRepositoryImpl(unittest.TestCase):
             }
         )
 
-        # Act: get all albums
         albums = self.repo.get_all_albums()
 
-        # Assert: check all albums
         self.assertEqual(len(albums), 2)
         self.assertEqual(albums[0].name, "Archives")
         self.assertEqual(albums[0].id, album_id_1)
         self.assertEqual(albums[0].parent_album_id, None)
-
         self.assertEqual(albums[1].name, "Photos")
         self.assertEqual(albums[1].id, album_id_2)
         self.assertEqual(albums[1].parent_album_id, album_id_1)
@@ -96,33 +97,26 @@ class TestAlbumsRepositoryImpl(unittest.TestCase):
             self.repo.get_album_by_id(album_id)
 
     def test_create_album(self):
-        # Prepare test data
         album_name = "New Album"
         parent_album_id = AlbumId(
             ObjectId("5f50c31e8a7d4b1c9c9b0b1c"), ObjectId("5f50c31e8a7d4b1c9c9b0b1d")
         )
 
-        # Test
         album = self.repo.create_album(album_name, parent_album_id)
 
-        # Assert
         self.assertEqual(album.name, album_name)
         self.assertEqual(album.parent_album_id, parent_album_id)
-
-        # Check if the album was actually inserted into the mock database
         inserted_album = self.mock_client["photos_drive"]["albums"].find_one(
             {"_id": album.id.object_id}
         )
         self.assertIsNotNone(inserted_album)
 
     def test_delete_many_albums__deletes_multiple_albums_successfully(self):
-        # Prepare test data
         album_ids = [
             AlbumId(MONGO_CLIENT_ID, ObjectId("5f50c31e8a7d4b1c9c9b0b1b")),
             AlbumId(MONGO_CLIENT_ID, ObjectId("5f50c31e8a7d4b1c9c9b0b1c")),
             AlbumId(MONGO_CLIENT_ID, ObjectId("5f50c31e8a7d4b1c9c9b0b1d")),
         ]
-
         for album_id in album_ids:
             self.mock_client["photos_drive"]["albums"].insert_one(
                 {
@@ -132,10 +126,8 @@ class TestAlbumsRepositoryImpl(unittest.TestCase):
                 }
             )
 
-        # Test
         self.repo.delete_many_albums(album_ids)
 
-        # Assert
         for album_id in album_ids:
             self.assertIsNone(
                 self.mock_client["photos_drive"]["albums"].find_one(
@@ -147,7 +139,6 @@ class TestAlbumsRepositoryImpl(unittest.TestCase):
         self.repo.delete_many_albums([])
 
     def test_delete_many_albums__with_non_existent_albums__raises_value_error(self):
-        # Prepare test data
         existing_album_id = AlbumId(
             MONGO_CLIENT_ID, ObjectId("5f50c31e8a7d4b1c9c9b0b1b")
         )
@@ -163,21 +154,36 @@ class TestAlbumsRepositoryImpl(unittest.TestCase):
             }
         )
 
-        # Test
         with self.assertRaises(ValueError):
             self.repo.delete_many_albums([existing_album_id, non_existent_album_id])
 
+    def test_delete_many_albums__with_wrong_client_id__raises_value_error(self):
+        existing_album_id = AlbumId(
+            MONGO_CLIENT_ID, ObjectId("5f50c31e8a7d4b1c9c9b0b1b")
+        )
+        wrong_client_album_id = AlbumId(
+            ObjectId(), ObjectId("5f50c31e8a7d4b1c9c9b0b1c")
+        )
+
+        self.mock_client["photos_drive"]["albums"].insert_one(
+            {
+                "_id": existing_album_id.object_id,
+                "name": "Existing Album",
+                "parent_album_id": None,
+            }
+        )
+
+        with self.assertRaises(ValueError):
+            self.repo.delete_many_albums([existing_album_id, wrong_client_album_id])
+
     def test_delete_album__with_valid_id(self):
-        # Prepare test data
         album_id = AlbumId(MONGO_CLIENT_ID, ObjectId("5f50c31e8a7d4b1c9c9b0b1b"))
         self.mock_client["photos_drive"]["albums"].insert_one(
             {"_id": album_id.object_id, "name": "Test Album"}
         )
 
-        # Test
         self.repo.delete_album(album_id)
 
-        # Assert
         deleted_album = self.mock_client["photos_drive"]["albums"].find_one(
             {"_id": album_id.object_id}
         )
@@ -191,8 +197,15 @@ class TestAlbumsRepositoryImpl(unittest.TestCase):
         ):
             self.repo.delete_album(album_id)
 
+    def test_delete_album__with_wrong_client_id(self):
+        album_id = AlbumId(ObjectId(), ObjectId("5f50c31e8a7d4b1c9c9b0b1b"))
+
+        with self.assertRaisesRegex(
+            ValueError, 'Album .* belongs to a different client'
+        ):
+            self.repo.delete_album(album_id)
+
     def test_update_album__with_new_fields(self):
-        # Prepare test data
         album_id = AlbumId(MONGO_CLIENT_ID, ObjectId("5f50c31e8a7d4b1c9c9b0b1b"))
         self.mock_client["photos_drive"]["albums"].insert_one(
             {
@@ -209,10 +222,8 @@ class TestAlbumsRepositoryImpl(unittest.TestCase):
             ),
         )
 
-        # Test
         self.repo.update_album(album_id, updated_fields)
 
-        # Assert
         updated_album = cast(
             Dict,
             self.mock_client["photos_drive"]["albums"].find_one(
@@ -233,6 +244,18 @@ class TestAlbumsRepositoryImpl(unittest.TestCase):
         )
 
         with self.assertRaisesRegex(ValueError, "Unable to update album .*"):
+            self.repo.update_album(album_id, updated_fields)
+
+    def test_update_album__with_wrong_client_id__throws_error(self):
+        album_id = AlbumId(ObjectId(), ObjectId("5f50c31e8a7d4b1c9c9b0b1b"))
+        updated_fields = UpdatedAlbumFields(
+            new_name="New Name",
+            new_parent_album_id=AlbumId(ObjectId(), ObjectId()),
+        )
+
+        with self.assertRaisesRegex(
+            ValueError, "Album .* belongs to a different client"
+        ):
             self.repo.update_album(album_id, updated_fields)
 
     def test_update_many_albums(self):
